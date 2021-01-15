31c1b371dd3abc0244c1c63786fe1c8b823bd3be
satanangelking <982883012@qq.com>
2021-01-15 16:09:08 +0800

    2

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="user">
-    <UserItem>
-      <img
-        src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1983364455,1831671477&fm=26&gp=0.jpg"
-        slot="head_photo"
-        alt=""
-      />
-      <span slot="name">satanangelking</span>
-      <img src="@/assets/user/Introduction.png" slot="I_icon" alt="" />
-      <span slot="introduce">简介</span>
-      <img src="@/assets/user/history.png" slot="H_icon" alt="" />
-      <span slot="history_purchase">采购记录</span>
-      <img src="@/assets/user/manager.png" slot="M_icon" alt="" />
-      <span slot="user_management">用户管理</span>
-      <img src="@/assets/user/setting.png" slot="S_icon" alt="" />
-      <span slot="setting">设置</span>
-    </UserItem>
+    <div class="photo">
+      <UserItem>
+        <img
+          src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1983364455,1831671477&fm=26&gp=0.jpg"
+          alt=""
+        />
+        <span>satanangelking</span>
+      </UserItem>
+    </div>
+    <div>
+      <UserItem
+        ><img src="@/assets/user/Introduction.png" alt="" />
+        <span>简介</span></UserItem
+      >
+    </div>
+    <div>
+      <UserItem
+        ><img src="@/assets/user/history.png" alt="" />
+        <span>采购记录</span></UserItem
+      >
+    </div>
+    <div>
+      <UserItem
+        ><img src="@/assets/user/manager.png" alt="" />
+        <span>用户管理</span></UserItem
+      >
+    </div>
+    <div>
+      <UserItem
+        ><img src="@/assets/user/setting.png" alt="" />
+        <span>设置</span></UserItem
+      >
+    </div>
+    <div @click="quit">
+      <UserItem
+        ><img src="@/assets/user/setting.png" alt="" />
+        <span>退出</span></UserItem
+      >
+    </div>
   </div>
 </template>
 <script>
@@ -24,25 +47,53 @@ export default {
   components: {
     UserItem
   },
-  mounted() {}
-  //   this.$api.post(
-  //     "user.do",
-  //     {
-  //       "": ""
-  //     },
-  //     response => {
-  //       if (response.status >= 200 && response.status < 300) {
-  //         console.log(response.data);
-  //       } else {
-  //         console.log(response.message);
-  //       }
-  //     }
-  //   );
-  // }
+  methods: {
+    quit() {
+      localStorage.removeItem('token')
+      window.location.reload()
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 #user {
   background: grey;
+  .photo {
+    img {
+      width: 50px;
+      height: 70px;
+    }
+    span {
+      top: 45px;
+    }
+  }
+  img {
+    width: 30px;
+    height: 30px;
+    margin: 10px 0;
+    border-radius: 50%;
+  }
+  div {
+    position: relative;
+    width: 100%;
+    text-align: left;
+    background: white;
+    margin: 20px 0;
+    img {
+      width: 30px;
+      height: 30px;
+      float: left;
+    }
+    span {
+      position: relative;
+      top: 15px;
+      margin-left: 20px;
+    }
+  }
+  div::after {
+    content: "";
+    clear: both; /*清除浮动*/
+    display: block; /*确保该元素是一个块级元素*/
+  }
 }
 </style>
--- src/views/user/useritem/UserItem.vue
+++ src/views/user/useritem/UserItem.vue
@@ -1,61 +1,10 @@
 <template>
   <div class="user-item">
-    <div class="user">
-      <slot name="head_photo" class="photo"></slot>
-      <slot name="name"></slot>
-    </div>
-    <div>
-      <slot name="I_icon"></slot>
-      <slot name="introduce"></slot>
-    </div>
-    <div>
-      <slot name="H_icon"></slot>
-      <slot name="history_purchase"></slot>
-    </div>
-    <div>
-      <slot name="M_icon"></slot>
-      <slot name="user_management"></slot>
-    </div>
-    <div>
-      <slot name="S_icon"></slot>
-      <slot name="setting"></slot>
-    </div>
+    <slot></slot>
   </div>
 </template>
 <style lang="scss" scoped>
 .user-item {
-  .user {
-    img {
-      width: 50px;
-      height: 70px;
-      margin-top: 20px;
-      border-radius: 50%;
-    }
-    span {
-      position: relative;
-      top: 45px;
-    }
-  }
-  div {
-    width: 100%;
-    text-align: left;
-    background: white;
-    margin: 20px 0;
-    img {
-      width: 30px;
-      height: 30px;
-      float: left;
-    }
-    span {
-      position: relative;
-      top: 5px;
-      margin-left: 20px;
-    }
-  }
-  div::after {
-    content: "";
-    clear: both; /*清除浮动*/
-    display: block; /*确保该元素是一个块级元素*/
-  }
+  height: 30px;
 }
 </style>
